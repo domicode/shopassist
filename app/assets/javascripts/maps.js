08016739147f5148f184b6a1c2b05be4238ccc08
Dominique Schuwey <domi@domidesign.ch>
2015-07-11 20:48:30 +0200

    add javascript for + button

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,34 @@
+$(document).ready( function () {
+  var displayMenu = false;
+  $('#toggleMenu').on('click', function () {
+    if (displayMenu) {
+      $('.menu').hide();
+      displayMenu = false;
+    } else {
+      $('#mainMenu').show();
+      displayMenu = true;
+    }
+  });
+
+  $('#findProduct').on('click', function () {
+    $('#mainMenu').hide();
+    $('#products').show();
+  });
+
+  $('#showGroceryList').on('click', function () {
+    $('#mainMenu').hide();
+    $('#groceryList').show();
+  });
+
+  $('.product').on('click', function () {
+    $('.pin').hide();
+    $('.product').removeClass('selected');
+    $('#'+$(event.target).attr('id')).toggle();
+    $(event.target).addClass('selected');
+    $('.addProduct').removeClass('selected');
+  });
+
+  $('.addProduct').on('click', function (event) {
+    $(event.target).parent().toggleClass('checked');
+  });
+});
