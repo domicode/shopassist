f6835dbcedbf7997d0e4662fbc50b7aaa848e9e9
Dominique Schuwey <domi@domidesign.ch>
2015-07-12 00:07:06 +0200

    make navigation work

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -2,5 +2,7 @@ $(document).ready(function () {
   $('.navigation-option').on('click', function (event) {
     $('.navigation-option').removeClass('select');
     $(event.target).toggleClass('select');
+    $('.main-menu-list').hide();
+    $('#'+$(event.target).attr('id')+'List').show();
   });
 });
