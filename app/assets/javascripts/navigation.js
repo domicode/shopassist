93dcfdfb1a63c119f35386d1401cffa05436df9f
Dominique Schuwey <domi@domidesign.ch>
2015-07-12 11:56:40 +0200

    add svg icons

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   $('.navigation-option').on('click', function (event) {
     $('.navigation-option').removeClass('select');
-    $(event.target).toggleClass('select');
+    $(event.target).closest('.navigation-option').toggleClass('select');
+    $('#navigation svg').each(function(key, value) { $(value).children('g').attr('fill', '#FFFFFF'); });
+    $(event.target).find('g').attr('fill', '#000000');
     $('.main-menu-list').hide();
-    $('#'+$(event.target).attr('id')+'List').animate({left:600, opacity:"show"}, 500);
+    $('#'+$(event.target).closest('.navigation-option').attr('id')+'List').animate({left:600, opacity:"show"}, 500);
     if (!$('#navigation .navigation-option').hasClass('select')) {
-      $('#'+$(event.target).attr('id')+'.navigation-option').addClass('select');
+      $('#'+$(event.target).closest('.navigation-option').attr('id')+'.navigation-option').addClass('select').find('g').attr('fill', '#000000');
       if (!$('#navigation .navigation-option').hasClass('select')) {
         $('#navigation #shops').addClass('select');
       }
