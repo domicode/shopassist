36705e2f90b374e5c1fc0c99cbfa279e4021acf8
Dominique Schuwey <domi@domidesign.ch>
2015-07-12 02:06:26 +0200

    finished navigation

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -4,5 +4,13 @@ $(document).ready(function () {
     $(event.target).toggleClass('select');
     $('.main-menu-list').hide();
     $('#'+$(event.target).attr('id')+'List').show();
+    if (!$('#navigation .navigation-option').hasClass('select')) {
+      $('#'+$(event.target).attr('id')+'.navigation-option').addClass('select');
+    }
+    $('.navigation-menu ul').hide();
+  });
+
+  $('.navigation-menu').on('click', function (event) {
+    $(event.target).siblings('ul').toggle();
   });
 });
